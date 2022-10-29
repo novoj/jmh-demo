a0ecbf942298db54f3245b03bbe2b261cfe1d1bc
JNO <novotny@fg.cz>
2022-10-29 13:46:23 +0200

    Final tweaks.

--- 05_profilers/src/main/java/net/novoj/BenchmarkRunner.java
+++ 05_profilers/src/main/java/net/novoj/BenchmarkRunner.java
@@ -1,6 +1,6 @@
 package net.novoj;
 
-import org.openjdk.jmh.profile.SafepointsProfiler;
+import org.openjdk.jmh.profile.StackProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
@@ -16,13 +16,13 @@ public class BenchmarkRunner {
 	public static void main(String[] args) throws Exception {
 		Options opt = new OptionsBuilder()
 			.include(JavaUuidTest.class.getName())
-			.forks(1)
+			.forks(0)
 			.threads(1)
 			.warmupTime(TimeValue.seconds(10))
 			.warmupIterations(1)
-			.timeout(TimeValue.seconds(10))
 			.measurementIterations(1)
-			.addProfiler(SafepointsProfiler.class)
+			.measurementTime(TimeValue.seconds(10))
+			.addProfiler(StackProfiler.class)
 			.build();
 
 		new Runner(opt).run();
